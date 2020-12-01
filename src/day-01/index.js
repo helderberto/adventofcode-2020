189b0628474389f29bf3276d76e4f67fd9359942
Helder Burato Berto <helder.burato@gmail.com>
2020-12-01 16:47:46 -0300

    refactor: update to use some instead of forEach
    It updates day 01 part1/2 to use some

--- src/day-01/index.js
+++ src/day-01/index.js
@@ -1,22 +1,39 @@
 import { readFile } from '../utils/file'
 import { multiply, sum } from '../utils/calc'
 
-export const solve = async () => {
+const splitNumbersFromFile = async () => {
   const file = await readFile('day-01/input.txt')
-  const arr = file.split('\n')
+  return file.split('\n')
+}
+
+export const solve = async () => {
+  const numbers = await splitNumbersFromFile()
   const when = 2020
+  let result
 
-  arr.forEach((n1, i) => {
-    arr.slice(i + 1).forEach((n2, j) => {
+  numbers.some((n1, i) =>
+    numbers.slice(i + 1).some((n2) => {
       if (sum(n1, n2) === when) {
-        console.log('day 01 - part 01 | result', multiply(n1, n2))
+        result = multiply(n1, n2)
+        return true
       }
+      return false
+    }),
+  )
 
-      arr.slice(j + 1).forEach((n3) => {
+  console.log('day 01 - part 01', result)
+
+  numbers.some((n1, i) =>
+    numbers.slice(i + 1).some((n2, j) =>
+      numbers.slice(j + 1).some((n3) => {
         if (sum(n1, n2, n3) === when) {
-          return console.log('day 01 - part 02 | result', multiply(n1, n2, n3))
+          result = multiply(n1, n2, n3)
+          return true
         }
-      })
-    })
-  })
+        return false
+      }),
+    ),
+  )
+
+  console.log('day 01 - part 02', result)
 }
