2d49eb6a6ff8d3e408029f435521b7b823fcac53
Helder Burato Berto <helder.burato@gmail.com>
2020-12-01 15:13:03 -0300

    feat: solve day 01 - part 1/2

--- src/day-01/index.js
+++ src/day-01/index.js
@@ -1,1 +1,22 @@
-export const sum = (x, y) => x + y
+import { readFile } from '../utils/file'
+import { multiply, sum } from '../utils/calc'
+
+export const solve = async () => {
+  const file = await readFile('day-01/input.txt')
+  const arr = file.split('\n')
+  const when = 2020
+
+  arr.forEach((n1, i) => {
+    arr.slice(i + 1).forEach((n2, j) => {
+      if (sum(n1, n2) === when) {
+        console.log('day 01 - part 01 | result', multiply(n1, n2))
+      }
+
+      arr.slice(j + 1).forEach((n3) => {
+        if (sum(n1, n2, n3) === when) {
+          return console.log('day 01 - part 02 | result', multiply(n1, n2, n3))
+        }
+      })
+    })
+  })
+}
--- src/day-01/test.js
+++ src/day-01/test.js
@@ -1,9 +1,3 @@
-import { sum } from '.'
-
-describe('sum', () => {
-  it('should sum 2 + 2 and returns 4', () => {
-    const total = sum(2, 2)
-
-    expect(total).toEqual(4)
-  })
+describe('Day 01', () => {
+  it('', () => {})
 })
