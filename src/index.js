97aeff4cc73b23c979ad1a57cd357659d30c4148
Helder Burato Berto <helder.burato@gmail.com>
2020-12-03 12:18:26 -0300

    feat: load solve03 into index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { solve as solve01 } from './day-01'
 import { solve as solve02 } from './day-02'
+import { solve as solve03 } from './day-03'
 ;(() => {
   solve01()
   solve02()
+  solve03()
 })()
